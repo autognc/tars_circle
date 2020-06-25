9defe0cd05333ae940cd99a5624fb0d8d05c62e3
Adam Trask <a.l.t.trask@gmail.com>
2020-06-25 15:25:21 -0500

    circleControl update

--- src/circleControl.cpp
+++ src/circleControl.cpp
@@ -28,11 +28,14 @@ double x,y, theta;
 double dXc,dYc,dthetaC,wheel1,wheel2,wheel3, wheel1speed, wheel2speed, wheel3speed;
 
 // Set radius of wheel and distance from CoM to wheel in meters
-float rWheel = 0.02;    // radius of wheel, m
-float D = 0.0307807;         // distance from CoM to wheel, m
+float rWheel = 0.05;    // radius of wheel, m
+float D = 0.175;         // distance from CoM to wheel, m
 
 geometry_msgs::Pose2D reftraj;
-ros::Time last_received;
+ros::Time last_received, LastWrite;
+ros::Duration writeDelay(1);
+
+
 std_msgs::Int16MultiArray twist;
 
 SerialPort ser("/dev/ttyS0");
@@ -64,24 +67,24 @@ double dYt(double time)
 
 
 
-/*
+
 void viconCallback(geometry_msgs::PoseStamped rover)
 {
     
-    x = rover.x;
-    y = rover.y;
-    theta = rover.theta + pi/6;
+    // x = rover.x;
+    // y = rover.y;
+    // theta = rover.theta + pi/6;
     last_received = ros::Time::now();
     
     
     //if (theta>=pi) theta -= 2*pi + pi/6;
-    if (theta<=0) theta += 2*pi;
+    // if (theta<=0) theta += 2*pi;
     
-    //if ((last_received >= beginTime) & (theta<=0)) {
-    //    theta += 2*pi;
-    //}
+    // //if ((last_received >= beginTime) & (theta<=0)) {
+    // //    theta += 2*pi;
+    // //}
     
-    if (last_received > loopTime) theta += 2*pi;
+    // if (last_received > loopTime) theta += 2*pi;
     
 
     
@@ -97,7 +100,7 @@ void viconCallback(geometry_msgs::PoseStamped rover)
     //if (yaw<-pi) yaw += 2*pi;
     
 }
-*/
+
 
 
 int speedCalc(float wheel_velocity)
@@ -267,7 +270,7 @@ int main(int argc, char** argv)
     //ref_pub = nh.advertise<geometry_msgs::PoseStamped>("/ref_traj",10);
     ref_pub = nh.advertise<geometry_msgs::Pose2D>("/ref_traj",10);
 
-    //vicon_sub = nh.subscribe("/open_base/pose/world", 100, viconCallback);
+    vicon_sub = nh.subscribe("/vicon", 100, viconCallback);
 
 
     double time, ref_time;
@@ -293,164 +296,140 @@ int main(int argc, char** argv)
     set_baudrate(2,128);
     set_baudrate(2,129);
     std::cout<<"Start"<<std::endl;
-    
-while(ros::ok()) {
-    d = ros::Time::now() - last_received;
-    time = d.toSec();
 
-    ref_time = last_received.toSec();
+    //while(ros::Time::now() < loopTime) {
+    while(ros::ok()) {
 
-    //Publish a reference trajectory to monitor on rviz
-    //reftraj.pose.position.x = Xt(ref_time);
-    //reftraj.pose.position.y = Yt(ref_time);
-    //reftraj.pose.position.z = 0;
-    //reftraj.pose.orientation.......
+        d = ros::Time::now() - last_received;
+        time = d.toSec();
 
-    reftraj.x = Xt(ref_time);
-    reftraj.y = Yt(ref_time);
+        ref_time = last_received.toSec();
 
-    ref_theta = ref_time*thetadot;
-    //if (ref_theta > pi) ref_theta -= 2*pi;
-    //if (ref_theta > 2*pi) ref_theta -= 2*pi;
-    reftraj.theta = ref_theta;
+        //Publish a reference trajectory to monitor on rviz
+        //reftraj.pose.position.x = Xt(ref_time);
+        //reftraj.pose.position.y = Yt(ref_time);
+        //reftraj.pose.position.z = 0;
+        //reftraj.pose.orientation.......
+
+        reftraj.x = Xt(ref_time);
+        reftraj.y = Yt(ref_time);
+
+        ref_theta = ref_time*thetadot;
+        //if (ref_theta > pi) ref_theta -= 2*pi;
+        //if (ref_theta > 2*pi) ref_theta -= 2*pi;
+        reftraj.theta = ref_theta;
 
 
-    //PID controller
-    // Proportional control:
-    Pxerr = (Xt(ref_time) - x);
-    Pyerr = (Yt(ref_time) - y);
-    Ptheta_err = (ref_theta - theta);
+        //PID controller
+        // Proportional control:
+        Pxerr = (Xt(ref_time) - x);
+        Pyerr = (Yt(ref_time) - y);
+        Ptheta_err = (ref_theta - theta);
 
-    // Integral control:
-    Ixerr += (Xt(ref_time) - x)/freq;
-    Iyerr += (Yt(ref_time) - y)/freq;
-    Itheta_err += (ref_theta - theta)/freq;
-    if (Ixerr>0.5) {
-        Ixerr = 0.5;
+        // Integral control:
+        Ixerr += (Xt(ref_time) - x)/freq;
+        Iyerr += (Yt(ref_time) - y)/freq;
+        Itheta_err += (ref_theta - theta)/freq;
+        if (Ixerr>0.5) {
+            Ixerr = 0.5;
+        }
+        else if (Ixerr<-0.5) { 
+            Ixerr = -0.5; 
+        }
+        if (Iyerr>0.5) {
+            Iyerr = 0.5;
+        }
+        else if (Iyerr<-0.5) {
+            Iyerr = -0.5;
+        }
+        if (Itheta_err>0.5) {
+            Itheta_err = 0.5;
+        }
+        else if (Itheta_err<-0.5) {
+            Itheta_err = -0.5;
+        }
+
+        // Derivative control:
+        Dxerr = (Xt(ref_time) - x) - OldErrX;
+        Dxerr = (Yt(ref_time) - y) - OldErrY;
+        Dtheta_err = (ref_theta - theta) - OldErrTheta;
+
+        OldErrX = Xt(ref_time) - x;
+        OldErrY = Yt(ref_time) - y;
+        OldErrTheta = ref_theta - theta;
+
+        // Set P, I, D values
+        double Kp, Ki, Kd;
+        Kp = 0;
+        Ki = 0;
+        Kd = 0;
+
+        //Calculate robot speed with PID feedback
+        //dXc = dXt(ref_time) + Kp*Pxerr + Ki*Ixerr + Kd*Dxerr;
+        dXc = 0;
+        dYc = dYt(ref_time) + Kp*Pyerr + Ki*Iyerr + Kd*Dyerr;
+        dthetaC = thetadot + Kp*Ptheta_err + Ki*Itheta_err + Kd*Dtheta_err;
+
+
+        //Calculate wheel speeds with vanHaendel model
+        // dXc is the xdot, dYc is the ydot, dthetaC is the thetadot
+
+        //Make the Jinv matrix
+        J11 = -sin(nu);
+        J12 = cos(nu);
+        J13 = D;
+        J21 = -sin(nu+alpha2);
+        J22 = cos(nu+alpha2);
+        J23 = D;
+        J31 = -sin(nu+alpha3);
+        J32 = cos(nu+alpha3);
+        J33 = D;
+
+        wheel1 = J11*dXc + J12*dYc + J13*dthetaC;       
+        wheel2 = J21*dXc + J22*dYc + J23*dthetaC;       
+        wheel3 = J31*dXc + J32*dYc + J33*dthetaC;      
+
+
+        wheel1speed = speedCalc(wheel1);
+        wheel2speed = speedCalc(wheel2);
+        wheel3speed = speedCalc(wheel3);
+
+        // wheel1speed = 64;
+        // wheel2speed = 64;
+        // wheel3speed = 64;
+
+
+        if (wheel1speed < 1) wheel1speed = 1;
+        if (wheel1speed > 127) wheel1speed = 127;
+
+        if (wheel2speed < 1) wheel2speed = 1;
+        if (wheel2speed > 127) wheel2speed = 127;
+
+        if (wheel3speed < 1) wheel3speed = 1;
+        if (wheel3speed > 127) wheel3speed = 127;
+
+
+       
+
+        if (ros::Time::now()-LastWrite>writeDelay)
+        {
+            control_motors_sep(6,wheel2speed,7,wheel1speed,128);
+            control_motors_sep(6,64,7,wheel3speed,129);
+
+            LastWrite = ros::Time::now();
+        }
+
+
+
+        //Publish necessary topics
+        ref_pub.publish(reftraj);
+        ros::spinOnce();
+        loop_rate.sleep();
+
+        
     }
-    else if (Ixerr<-0.5) { 
-        Ixerr = -0.5; 
-    }
-    if (Iyerr>0.5) {
-        Iyerr = 0.5;
-    }
-    else if (Iyerr<-0.5) {
-        Iyerr = -0.5;
-    }
-    if (Itheta_err>0.5) {
-        Itheta_err = 0.5;
-    }
-    else if (Itheta_err<-0.5) {
-        Itheta_err = -0.5;
-    }
-
-    // Derivative control:
-    Dxerr = (Xt(ref_time) - x) - OldErrX;
-    Dxerr = (Yt(ref_time) - y) - OldErrY;
-    Dtheta_err = (ref_theta - theta) - OldErrTheta;
-
-    OldErrX = Xt(ref_time) - x;
-    OldErrY = Yt(ref_time) - y;
-    OldErrTheta = ref_theta - theta;
-
-    // Set P, I, D values
-    double Kp, Ki, Kd;
-    Kp = 0;
-    Ki = 0;
-    Kd = 0;
-
-    //Calculate robot speed with PID feedback
-    dXc = dXt(ref_time) + Kp*Pxerr + Ki*Ixerr + Kd*Dxerr;
-    dYc = dYt(ref_time) + Kp*Pyerr + Ki*Iyerr + Kd*Dyerr;
-    dthetaC = thetadot + Kp*Ptheta_err + Ki*Itheta_err + Kd*Dtheta_err;
-
-
-    //Calculate wheel speeds with vanHaendel model
-    // dXc is the xdot, dYc is the ydot, dthetaC is the thetadot
-
-    //Make the Jinv matrix
-    J11 = -sin(nu);
-    J12 = cos(nu);
-    J13 = D;
-    J21 = -sin(nu+alpha2);
-    J22 = cos(nu+alpha2);
-    J23 = D;
-    J31 = -sin(nu+alpha3);
-    J32 = cos(nu+alpha3);
-    J33 = D;
-
-    wheel1 = J11*dXc + J12*dYc + J13*dthetaC;       
-    wheel2 = J21*dXc + J22*dYc + J23*dthetaC;       
-    wheel3 = J31*dXc + J32*dYc + J33*dthetaC;      
-
-
-    wheel1speed = speedCalc(wheel1);
-    wheel2speed = speedCalc(wheel2);
-    wheel3speed = speedCalc(wheel3);
-
-
-    if (wheel1speed < 1) wheel1speed = 1;
-    if (wheel1speed > 127) wheel1speed = 127;
-
-    if (wheel2speed < 1) wheel2speed = 1;
-    if (wheel2speed > 127) wheel2speed = 127;
-
-    if (wheel3speed < 1) wheel3speed = 1;
-    if (wheel3speed > 127) wheel3speed = 127;
-
-
-    control_motors_sep(6,wheel1speed,7,wheel2speed,128);
-    control_motors_sep(6,64,7,wheel3speed,129);
-
-
-    //Saturate wheel speeds and push into topic type
-    /*
-    if (wheel1 < 1) wheel1 = 1;
-    if (wheel1 > 127) wheel1 = 127;
-
-    if (wheel2 < 128) wheel2 = 128;
-    if (wheel2 > 255) wheel2 = 255;
-
-    if (wheel3 < 1) wheel3 = 1;
-    if (wheel3 > 127) wheel3 = 127;
-    */
-
-    /*
-    twist.data.clear();
-    twist.data.push_back(wheel1);
-    twist.data.push_back(wheel2);
-    twist.data.push_back(wheel3);
-    twist.data.push_back(0);
-    */
-
-
-
-
-    //Publish necessary topics
-    ref_pub.publish(reftraj);
-    ros::spinOnce();
-    loop_rate.sleep();
-
-    
- }
-
-
-
-/*
-while (ros::ok())
-{
-
-
-
-    omni_command.movement = 3;
-    omni_command.wheel.v_left = -0.09827304;
-    omni_command.wheel.v_back = 0.21588622;
-    omni_command.wheel.v_right = -0.09827304;
-
-    command_pub.publish(omni_command);
-
-    ros::spinOnce();
-    loop_rate.sleep();
-}
-*/
+control_motors_sep(6,64,7,64,129);
+control_motors_sep(6,64,7,64,128);
+ser.Close();
+return 0;
 }
